f3cfff4d5e2af2b17642c034ae581f8193a5da56
xlboy <984492408@qq.com>
2021-04-12 21:37:39 +0800

    IM离线消息的添加

---
+++ service/app/entities/imOfflineMessage.entity.ts
@@ -0,0 +1,30 @@
+import { ContentType } from 'app/constants/socket'
+import { MinLength, IsNotEmpty } from 'class-validator'
+import {
+  Entity,
+  Column,
+  ManyToOne
+} from 'typeorm'
+import BaseEntity from './common/base.entity'
+import Userinfo from './userinfo.entity'
+@Entity('im_offline_message')
+export default class ImOfflineMessage extends BaseEntity {
+
+  constructor(data?: { [k in keyof ImOfflineMessage]?: ImOfflineMessage[k] }) {
+    super()
+    if (data) {
+      Object.keys(data).forEach(key => {
+        this[key] = data[key]
+      })
+    }
+  }
+
+  @IsNotEmpty({ message: 'content不可为空' })
+  @Column("simple-json")
+  content: { contentType: ContentType; content: any };
+
+
+  @ManyToOne(() => Userinfo, userinfo => userinfo.planInfo)
+  userinfo: Userinfo;
+
+}
